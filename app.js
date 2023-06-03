4f725af0cd332ce7fed3307f5628760c55685c83
keyxcode <keyxcode@gmail.com>
2023-06-03 14:28:54 +0700

    Create dummy health and version endpoints

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/version', (_req, res) => {
+  res.send('1')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5001')
 })
