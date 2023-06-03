bd26f4a5a6ff9c4065c2cdf45cb75439d762ce2d
keyxcode <keyxcode@gmail.com>
2023-06-03 14:39:52 +0700

    Solution 11.12: revert app status to good health

--- app.js
+++ app.js
@@ -10,12 +10,10 @@ app.get('/version', (_req, res) => {
   res.send('1')
 })
 
-app.get('/health', (req, res) => {
+app.get('/health', (_req, res) => {
   res.send('ok')
 })
 
 app.listen(PORT, () => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   console.log('server started on port 5001')
 })
